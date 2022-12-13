e773e0c1f7f94e3c2e1b161a4c34d98913965581
sebas <sebas2.0he@gmail.com>
2022-12-13 14:19:32 -0500

    Se añadieron las funcionalidades de abrir menus con JS

--- main.js
+++ main.js
@@ -1,15 +1,36 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+
 const burguerMenu = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 
-menuEmail.addEventListener('click', toggleDesktopMenu)
-burguerMenu.addEventListener('click', toggleMobileMenu)
+const iconCart = document.querySelector('.navbar-shopping-cart')
+const productsInCart = document.querySelector('.product-detail')
 
+
+menuEmail.addEventListener('click', toggleDesktopMenu)
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive')
+    if(!productsInCart.classList.contains('inactive')){
+        productsInCart.classList.toggle('inactive')
+    }
 }
 
+burguerMenu.addEventListener('click', toggleMobileMenu)
 function toggleMobileMenu(){
     mobileMenu.classList.toggle('inactive')
+    if(!productsInCart.classList.contains('inactive')){
+        productsInCart.classList.toggle('inactive')
+    }
+}
+
+iconCart.addEventListener('click', toggleCart)
+function toggleCart(){
+    productsInCart.classList.toggle("inactive")
+    if(!mobileMenu.classList.contains('inactive')){
+        mobileMenu.classList.toggle('inactive')
+    }
+    if(!desktopMenu.classList.toggle('inactive')){
+        desktopMenu.classList.toggle('inactive')
+    }
 }
